b0d065fb8be690d6cee93f468a577398ddd02aff
dereksodo <40626515+dereksodo@users.noreply.github.com>
2019-07-27 17:52:11 +0800

    add code for 7-27

--- Ch5/5.4.1.cpp
+++ Ch5/5.4.1.cpp
@@ -25,10 +25,50 @@ typedef long long ll;
 #else
 	#define debug(...)
 #endif
+const int maxn = 105;
+map<string,int> mp;
+int dp[maxn][maxn],G[maxn][maxn];
 int main(int argc, char const *argv[])
 {
-	// freopen("tour.in","r",stdin);
-	// freopen("tour.out","w",stdout);
-	
+	freopen("tour.in","r",stdin);
+	freopen("tour.out","w",stdout);
+	int n,m;
+	cin>>n>>m;
+	for(int i = 1;i <= n; ++i)
+	{
+		string s;
+		cin>>s;
+		mp[s] = i;
+	}
+	memset(G,0x00,sizeof(G));
+	for(int i = 0;i < m; ++i)
+	{
+		string s,t;
+		cin>>s>>t;
+		G[mp[s]][mp[t]] = G[mp[t]][mp[s]] = 1;
+	}
+	dp[1][1] = 1;
+	for(int i = 1;i < n; ++i)
+	{
+		for(int j = i + 1;j <= n; ++j)
+		{
+			for(int k = 1;k < j; ++k)
+			{
+				if(G[j][k] && dp[i][k])
+				{
+					dp[i][j] = dp[j][i] = max(dp[i][j],dp[i][k] + 1);
+				}
+			}
+		}
+	}
+	int ans = 1;
+	for(int i = 1;i <= n; ++i)
+	{
+		if(G[n][i])
+		{
+			ans = max(ans,dp[n][i]);
+		}
+	}
+	cout<<ans<<endl;
 	return 0;
 }
